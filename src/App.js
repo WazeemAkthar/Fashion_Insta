0cbad680900a66126dd5f3bdcb05e837daa2415e
Wazeem Akthar <aktharwazeem@gmail.com>
2023-10-09 15:46:04 +0530

    fashioninsta

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import Header from "./Components/Header";
 import Herosection from "./Components/Herosection";
 import Topvalue from "./Components/Topvalue";
-// import Collection from "./Components/Collection";
-// import Shopnow from "./Components/Shopnow";
-// import Contectus from "./Components/Contectus";
-// import Joinus from "./Components/Joinus";
+import Collection from "./Components/Collection";
+import Shopnow from "./Components/Shopnow";
+import Contectus from "./Components/Contectus";
+import Joinus from "./Components/Joinus";
 import Explore from "./Components/Explore";
 import Footer from "./Components/Footer";
 import "./App.css";
@@ -17,15 +17,15 @@ function App() {
       <Herosection />
        <Topvalue />
 
-       
+
        <Explore/>
        <Footer/>
-     {/* <Collection />
+     <Collection />
       <Shopnow />
       <Contectus />
        <Joinus/>
     
-          */}
+         
     </>
   );
 }
